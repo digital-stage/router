8b53a5499dae628a198e9a68491b86d9960f5d13
delude88 <tobias.hegemann@googlemail.com>
2021-01-06 00:40:49 +0100

    feature: added more env vars

--- src/env.ts
+++ src/env.ts
@@ -8,15 +8,21 @@ const {
     IP_V4,
     IP_V6,
     LISTEN_IP,
-    ANNOUNCED_IP
+    ANNOUNCED_IP,
+    WS_PREFIX,
+    REST_PREFIX,
+    DOMAIN,
+    ROOT_PATH
 } = process.env;
 
 const PORT = parseInt(process.env.PORT, 10);
+const PUBLIC_PORT = parseInt(process.env.PUBLIC_PORT, 10);
 const RTC_MIN_PORT = parseInt(process.env.RTC_MIN_PORT, 10);
 const RTC_MAX_PORT = parseInt(process.env.RTC_MAX_PORT, 10);
 const OV_MIN_PORT = parseInt(process.env.OV_MIN_PORT, 10);
 const OV_MAX_PORT = parseInt(process.env.OV_MAX_PORT, 10);
 const CONNECTIONS_PER_CPU = parseInt(process.env.CONNECTIONS_PER_CPU, 10);
+const USE_IPV6 = process.env.USE_IPV6 ? process.env.USE_IPV6 === "true" : false;
 
 
 const MEDIASOUP_CONFIG = require('./config').default;
@@ -30,9 +36,15 @@ export {
     API_KEY,
     IP_V4,
     IP_V6,
+    USE_IPV6,
     PORT,
+    PUBLIC_PORT,
     CONNECTIONS_PER_CPU,
     MEDIASOUP_CONFIG,
     LISTEN_IP,
-    ANNOUNCED_IP
+    ANNOUNCED_IP,
+    WS_PREFIX,
+    REST_PREFIX,
+    DOMAIN,
+    ROOT_PATH
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,14 @@
 import Router from "./model/Router";
 import {
-    ANNOUNCED_IP,
+    ANNOUNCED_IP, DOMAIN,
     IP_V4,
     IP_V6,
     LISTEN_IP,
     MEDIASOUP_CONFIG,
     OV_MAX_PORT,
-    PORT,
+    PORT, PUBLIC_PORT, REST_PREFIX, ROOT_PATH,
     RTC_MAX_PORT,
-    RTC_MIN_PORT
+    RTC_MIN_PORT, USE_IPV6, WS_PREFIX
 } from "./env";
 import * as publicIp from "public-ip";
 import {MediasoupConfiguration} from "./services/MediasoupService";
@@ -30,19 +30,18 @@ const getDefaultMediasoupConfig = (ipv4: string): MediasoupConfiguration => {
 
 const getInitialRouter = async (): Promise<Omit<Router, "_id">> => {
     const ipv4: string = IP_V4 || await publicIp.v4()
-    const ipv6: string = IP_V6 || await publicIp.v6()
-    const router: Omit<Router, "_id"> = {
-        wsPrefix: "ws",
-        restPrefix: "",
-        url: "localhost",
-        path: "",
+    const ipv6: string = USE_IPV6 ? IP_V6 || await publicIp.v6() : undefined;
+    return {
+        wsPrefix: WS_PREFIX,
+        restPrefix: REST_PREFIX,
+        url: DOMAIN,
+        path: ROOT_PATH,
         ipv4: ipv4,
         ipv6: ipv6,
-        port: PORT,
+        port: PUBLIC_PORT,
         availableRTCSlots: (RTC_MAX_PORT - RTC_MIN_PORT),
         availableOVSlots: (OV_MAX_PORT - RTC_MIN_PORT),
     };
-    return router;
 }
 
 export {
